42b3ded0006187a94a436e2c41b4facb565a63e8
Prashant Kumar <tumsab7777@gmail.com>
2025-03-06 20:17:25 +0530

    allowed host

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [name , setName] = useState("")
 
 function handleClick(){
-  axios.get(`https://younglabs-backend/api/greet?name=${name}`).then((res)=>{
+  axios.get(`https://younglabs-backend.tumsab.xyz/api/greet?name=${name}`).then((res)=>{
     setResponse(res.data.message)
   }).catch((err)=>{
     console.log(err)
