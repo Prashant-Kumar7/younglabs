2c5ece4417c32dc14b597552fa8a50e2556bfa60
Prashant Kumar <tumsab7777@gmail.com>
2025-03-06 20:06:07 +0530

    allowed host

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  server: {
+    host: true, // Allows external access
+    allowedHosts: ['younglabs.tumsab.xyz'], // Add your domain here
+  }
+
 })
