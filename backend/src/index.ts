43260819880509ea429c4ba47f15b4b35400e82e
Prashant Kumar <tumsab7777@gmail.com>
2025-03-06 20:11:19 +0530

    allowed host

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,9 +7,14 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors({
-    origin : "https://younglabs.tumsab.xyz"
-}))
+app.use(
+    cors({
+      origin: "https://younglabs.tumsab.xyz",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: false, 
+    })
+  );
 app.use(express.json())
 
 app.get("/api/greet", (req: Request, res: Response) => {
