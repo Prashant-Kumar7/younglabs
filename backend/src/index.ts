59fe773814258f0a53c2998212a2a2167d4f7923
Prashant Kumar <tumsab7777@gmail.com>
2025-03-06 19:41:06 +0530

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import express, {Response , Request} from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors({
+    origin : "https://younglabs.tumsab.xyz"
+}))
+app.use(express.json())
+
+app.get("/api/greet", (req: Request, res: Response) => {
+    const name = req.query.name as string;
+    res.json({
+        "message": `Hello, ${name}! Welcome to Younglabs.`
+    })
+});
+
+app.listen(port , ()=>{
+    console.log(`server is running on port ${port}`)
+})
---
+++ react-app/src/App.tsx
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import { useState } from 'react'
+
+
+function App() {
+  const [response , setResponse] = useState("")
+  const [name , setName] = useState("")
+
+function handleClick(){
+  axios.get(`http://localhost:3000/api/greet?name=${name}`).then((res)=>{
+    setResponse(res.data.message)
+  }).catch((err)=>{
+    console.log(err)
+  })
+}
+
+  return (
+    <div className='bg-zinc-900 h-screen w-screen flex justify-center items-center gap-6'>
+      <div className='flex flex-col justify-center'>
+        <div className='flex gap-4 items-center'>
+          <label className='text-gray-200' htmlFor="name">Name </label>
+          <input onChange={(e)=>setName(e.target.value)} type="text" name='name' className='border-0 px-2 py-1 text-gray-200 outline-none focus:outline-none bg-zinc-800' placeholder='Enter Name' required/>
+          <button onClick={handleClick} className='p-1 rounded-md text-gray-200 hover:cursor-pointer hover:bg-indigo-800 bg-indigo-700'>Submit</button>
+        </div>
+        <span className='text-gray-200 mt-10 text-xl'>{response}</span>
+      </div>
+    </div>
+  )
+}
+
+export default App
